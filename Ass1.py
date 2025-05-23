49b75fe7ab5df456c8d9e634950d8db2d1e13559
Lochan78 <148063231+Lochan78@users.noreply.github.com>
2025-05-23 20:59:11 +0530

    Update Ass1.py

--- Ass1.py
+++ Ass1.py
@@ -1,3 +1,12 @@
 rows = 5
+print("Lower Triangular Pattern:")
 for i in range(1, rows + 1):
     print("* " * i)
+
+print("\nUpper Triangular Pattern:")
+for i in range(rows, 0, -1):
+    print("* " * i)
+
+print("\nPyramid Pattern:")
+for i in range(1, rows + 1):
+    print(" " * (rows - i) + "* " * i)
